a094fc12ab5dbe0f533f32c9b26d2bc0be78d598
Divyesh Malhtora <zeitgeist117@proton.me>
2022-10-01 01:12:10 +1000

    modified:   .config/conky/todo 	modified:   .config/suckless/dwmblocks/blocks.def.h 	modified:   .config/suckless/dwmblocks/dwmblocks 	modified:   .config/suckless/dwmblocks/scripts/forecast.sh 	modified:   .newsboat/cache.db 	modified:   .newsboat/urls

--- .config/suckless/dwmblocks/blocks.def.h
+++ .config/suckless/dwmblocks/blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"[", "/home/nightwing/.config/suckless/dwmblocks/scripts/forecast.sh", 10, 5},
+	{"[", "/home/nightwing/.config/suckless/dwmblocks/scripts/forecast.sh", 360, 5},
 	{"﬙ ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
 	{" ", "top -bn1 | grep Cpu | awk '{print $2}'", 5, 0},
 	{" ", "df -h /home/nightwing| grep -vE '^Filesystem' | awk '{print $5, $4 }'", 3600, 0},
