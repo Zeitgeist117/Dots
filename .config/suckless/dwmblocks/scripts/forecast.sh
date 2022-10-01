8ba76cd6bd308351bbed60a35af64957a8463fe3
Divyesh Malhtora <zeitgeist117@proton.me>
2022-10-01 15:37:54 +1000

    modified:   .config/qutebrowser/autoconfig.yml 	modified:   .config/suckless/dwmblocks/scripts/forecast.sh

--- .config/suckless/dwmblocks/scripts/forecast.sh
+++ .config/suckless/dwmblocks/scripts/forecast.sh
@@ -16,6 +16,16 @@ showweather() { printf "%s" "$(sed '16q;d' "$weatherreport" |
 	grep -wo "[0-9]*%" | sort -rn | sed "s/^/ /g;1q" | tr -d '\n')"
 sed '13q;d' "$weatherreport" | grep -o "m\\([-+]\\)*[0-9]\\+" | sed 's/+//g' | sort -n -t 'm' -k 2n | sed -e 1b -e '$!d' | tr '\n|m' ' ' | awk '{print "  " $1 "°"," " $2 "°"}' ;}
 
+case $BLOCK_BUTTON in
+	1) setsid -f "$TERMINAL" -e less -Srf "$weatherreport" ;;
+	2) getforecast && showweather ;;
+	3) notify-send "🌈 Weather module" "\- Left click for full forecast.
+- Middle click to update forecast.
+☔: Chance of rain/snow
+🥶: Daily low
+🌞: Daily high" ;;
+	6) "$TERMINAL" -e "$EDITOR" "$0" ;;
+esac
 # The test if our forcecast is updated to the day. If it isn't download a new
 # weather report from wttr.in with the above function.
 [ "$(stat -c %y "$weatherreport" 2>/dev/null | cut -d' ' -f1)" = "$(date '+%Y-%m-%d')" ] ||
