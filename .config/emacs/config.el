80a6adadddbcbca5b0bc6f70200e7b7ee6b94db1
zeitgeist117 <zeitgeist117@proton.me>
2023-10-26 02:15:16 +1100

    Changes to be committed: 	modified:   .Xresources 	modified:   .config/emacs/README.org 	modified:   .config/emacs/config.el 	modified:   .config/xmonad/README.org 	modified:   README.org
    Untracked files:

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -525,7 +525,7 @@
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
   ;; Sets the default theme to load!!! 
-  (load-theme 'doom-gruvbox t)
+  (load-theme 'doom-gruvbox-material t)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; Corrects (and improves) org-mode's native fontification.
