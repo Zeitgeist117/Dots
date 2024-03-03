ee8dc0643338121ef4c5cda9cea6c7702db1546f
zeitgeist117 <zeitgeist117@proton.me>
2024-03-03 22:37:09 +1100

    Changes to be committed: modified:   emacs/README.org modified:   emacs/config.el new file:   emacs/images/emacs-dash.jpg new file:   emacs/images/emacs-dash.png new file:   emacs/images/eva.txt new file:   emacs/images/lambda.txt
    Changes not staged for commit:
    modified:   lf/lfrc
    deleted:    nvim/after/plugin/bufferline.lua
    deleted:    nvim/after/plugin/cmp.lua
    deleted:    nvim/after/plugin/colors.lua
    deleted:    nvim/after/plugin/dashboard.lua
    deleted:    nvim/after/plugin/fugitive.lua
    deleted:    nvim/after/plugin/harpoon.lua
    deleted:    nvim/after/plugin/lsp.lua
    deleted:    nvim/after/plugin/mason.lua
    deleted:    nvim/after/plugin/neotree.lua
    deleted:    nvim/after/plugin/statusbar.lua
    deleted:    nvim/after/plugin/telescope.lua
    deleted:    nvim/after/plugin/treesitter.lua
    deleted:    nvim/after/plugin/vimwiki.lua
    deleted:    nvim/after/plugin/zenmode.lua
    modified:   nvim/init.lua
    deleted:    nvim/init.txt
    deleted:    nvim/lazy-lock.json
    deleted:    nvim/lua/keymaps.lua
    deleted:    nvim/lua/options.lua
    deleted:    nvim/lua/pluginlist.lua
    deleted:    nvim/lua/plugins.lua
    modified:   ../.zshrc
    
    Untracked files:
    nvim.lazy/
    nvim/LICENSE.md
    nvim/README.md
    nvim/doc/
    nvim/lua/custom/
    nvim/lua/kickstart/

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -62,190 +62,194 @@
 (setq bookmark-save-flag 1) ; save bookmarks automatically
 
 (use-package general
-      :config
-      (general-evil-setup)
+	:config
+	(general-evil-setup)
 
-      ;; set up 'SPC' as the global leader key
-      (general-create-definer zg/leader-keys
-	:states '(normal insert visual emacs)
-	:keymaps 'override
-	:prefix "SPC" ;; set leader
-	:global-prefix "M-SPC") ;; access leader in insert mode
+	;; set up 'SPC' as the global leader key
+	(general-create-definer zg/leader-keys
+	  :states '(normal insert visual emacs)
+	  :keymaps 'override
+	  :prefix "SPC" ;; set leader
+	  :global-prefix "M-SPC") ;; access leader in insert mode
 
-      (zg/leader-keys
-	"SPC" '(counsel-M-x :wk "Counsel M-x")
-	"." '(find-file :wk "Find file")
-	"=" '(perspective-map :wk "Perspective") ;; Lists all the perspective keybindings
-	"c c" '(comment-line :wk "Comment lines")
-	"u" '(universal-argument :wk "Universal argument"))
+	(zg/leader-keys
+	  "SPC" '(counsel-M-x :wk "Counsel M-x")
+	  "." '(find-file :wk "Find file")
+	  "=" '(perspective-map :wk "Perspective") ;; Lists all the perspective keybindings
+	  "c c" '(comment-line :wk "Comment lines")
+	  "u" '(universal-argument :wk "Universal argument"))
 
-      (zg/leader-keys
-	"f" '(:ignore t :wk "Files")    
-	"f c" '((lambda () (interactive)
-			      (find-file "~/.config/emacs/README.org")) 
-			:wk "Open emacs README.org")
-	"f e" '((lambda () (interactive)
-			      (dired "~/.config/emacs/")) 
-			:wk "Open user-emacs-directory in dired")
-	"f d" '(find-grep-dired :wk "Search for string in files in DIR")
-	"f g" '(counsel-grep-or-swiper :wk "Search for string current file")
-	"f i" '((lambda () (interactive)
-			      (find-file "~/.config/emacs/init.el")) 
-			:wk "Open emacs init.el")
-	"f j" '(counsel-file-jump :wk "Jump to a file below current directory")
-	"f l" '(counsel-locate :wk "Locate a file")
-	"f r" '(counsel-recentf :wk "Find recent files")
-	"f u" '(sudo-edit-find-file :wk "Sudo find file")
-	"f U" '(sudo-edit :wk "Sudo edit file"))
+	(zg/leader-keys
+	  "f" '(:ignore t :wk "Files")    
+	  "f c" '((lambda () (interactive)
+				(find-file "~/.config/emacs/README.org")) 
+			  :wk "Open emacs README.org")
+	  "f e" '((lambda () (interactive)
+				(dired "~/.config/emacs/")) 
+			  :wk "Open user-emacs-directory in dired")
+	  "f d" '(find-grep-dired :wk "Search for string in files in DIR")
+	  "f g" '(counsel-grep-or-swiper :wk "Search for string current file")
+	  "f i" '((lambda () (interactive)
+				(find-file "~/.config/emacs/init.el")) 
+			  :wk "Open emacs init.el")
+	  "f j" '(counsel-file-jump :wk "Jump to a file below current directory")
+	  "f l" '(counsel-locate :wk "Locate a file")
+	  "f r" '(counsel-recentf :wk "Find recent files")
+	  "f u" '(sudo-edit-find-file :wk "Sudo find file")
+	  "f U" '(sudo-edit :wk "Sudo edit file"))
 
-      (zg/leader-keys
-	"b" '(:ignore t :wk "Bookmarks/Buffers")
-	"b b" '(switch-to-buffer :wk "Switch to buffer")
-	"b c" '(clone-indirect-buffer :wk "Create indirect buffer copy in a split")
-	"b C" '(clone-indirect-buffer-other-window :wk "Clone indirect buffer in new window")
-	"b d" '(bookmark-delete :wk "Delete bookmark")
-	"b i" '(ibuffer :wk "Ibuffer")
-	"b k" '(kill-current-buffer :wk "Kill current buffer")
-	"b K" '(kill-some-buffers :wk "Kill multiple buffers")
-	"b l" '(list-bookmarks :wk "List bookmarks")
-	"b m" '(bookmark-set :wk "Set bookmark")
-	"b n" '(next-buffer :wk "Next buffer")
-	"b p" '(previous-buffer :wk "Previous buffer")
-	"b r" '(revert-buffer :wk "Reload buffer")
-	"b R" '(rename-buffer :wk "Rename buffer")
-	"b s" '(basic-save-buffer :wk "Save buffer")
-	"b S" '(save-some-buffers :wk "Save multiple buffers")
-	"b w" '(bookmark-save :wk "Save current bookmarks to bookmark file"))
+	(zg/leader-keys
+	  "b" '(:ignore t :wk "Bookmarks/Buffers")
+	  "b b" '(switch-to-buffer :wk "Switch to buffer")
+	  "b c" '(clone-indirect-buffer :wk "Create indirect buffer copy in a split")
+	  "b C" '(clone-indirect-buffer-other-window :wk "Clone indirect buffer in new window")
+	  "b d" '(bookmark-delete :wk "Delete bookmark")
+	  "b i" '(ibuffer :wk "Ibuffer")
+	  "b k" '(kill-current-buffer :wk "Kill current buffer")
+	  "b K" '(kill-some-buffers :wk "Kill multiple buffers")
+	  "b l" '(list-bookmarks :wk "List bookmarks")
+	  "b m" '(bookmark-set :wk "Set bookmark")
+	  "b n" '(next-buffer :wk "Next buffer")
+	  "b p" '(previous-buffer :wk "Previous buffer")
+	  "b r" '(revert-buffer :wk "Reload buffer")
+	  "b R" '(rename-buffer :wk "Rename buffer")
+	  "b s" '(basic-save-buffer :wk "Save buffer")
+	  "b S" '(save-some-buffers :wk "Save multiple buffers")
+	  "b w" '(bookmark-save :wk "Save current bookmarks to bookmark file"))
 
-      (zg/leader-keys
-	"e" '(:ignore t :wk "Eshell/Evaluate")    
-	"e b" '(eval-buffer :wk "Evaluate elisp in buffer")
-	"e d" '(eval-defun :wk "Evaluate defun containing or after point")
-	"e e" '(eval-expression :wk "Evaluate and elisp expression")
-	"e h" '(counsel-esh-history :which-key "Eshell history")
-	"e l" '(eval-last-sexp :wk "Evaluate elisp expression before point")
-	"e r" '(eval-region :wk "Evaluate elisp in region")
-	"e R" '(eww-reload :which-key "Reload current page in EWW")
-	"e s" '(eshell :which-key "Eshell")
-	"e w" '(eww :which-key "EWW emacs web wowser"))
+	(zg/leader-keys
+	  "e" '(:ignore t :wk "Eshell/Evaluate")    
+	  "e b" '(eval-buffer :wk "Evaluate elisp in buffer")
+	  "e d" '(eval-defun :wk "Evaluate defun containing or after point")
+	  "e e" '(eval-expression :wk "Evaluate and elisp expression")
+	  "e h" '(counsel-esh-history :which-key "Eshell history")
+	  "e l" '(eval-last-sexp :wk "Evaluate elisp expression before point")
+	  "e r" '(eval-region :wk "Evaluate elisp in region")
+	  "e R" '(eww-reload :which-key "Reload current page in EWW")
+	  "e s" '(eshell :which-key "Eshell")
+	  "e w" '(eww :which-key "EWW emacs web wowser"))
 
-      (zg/leader-keys
-	"g" '(:ignore t :wk "Git")    
-	"g /" '(magit-displatch :wk "Magit dispatch")
-	"g p" '(magit-push :wk "Magit push")
-	"g ." '(magit-file-displatch :wk "Magit file dispatch")
-	"g b" '(magit-branch-checkout :wk "Switch branch")
-	"g c" '(:ignore t :wk "Create") 
-	"g c b" '(magit-branch-and-checkout :wk "Create branch and checkout")
-	"g c c" '(magit-commit-create :wk "Create commit")
-	"g c f" '(magit-commit-fixup :wk "Create fixup commit")
-	"g C" '(magit-clone :wk "Clone repo")
-	"g f" '(:ignore t :wk "Find") 
-	"g f c" '(magit-show-commit :wk "Show commit")
-	"g f f" '(magit-find-file :wk "Magit find file")
-	"g f g" '(magit-find-git-config-file :wk "Find gitconfig file")
-	"g F" '(magit-fetch :wk "Git fetch")
-	"g g" '(magit-status :wk "Magit status")
-	"g i" '(magit-init :wk "Initialize git repo")
-	"g l" '(magit-log-buffer-file :wk "Magit buffer log")
-	"g r" '(vc-revert :wk "Git revert file")
-	"g s" '(magit-stage-file :wk "Git stage file")
-	"g t" '(git-timemachine :wk "Git time machine")
-	"g u" '(magit-stage-file :wk "Git unstage file"))
+	(zg/leader-keys
+	  "g" '(:ignore t :wk "Git")    
+	  "g /" '(magit-displatch :wk "Magit dispatch")
+	  "g p" '(magit-push :wk "Magit push")
+	  "g ." '(magit-file-displatch :wk "Magit file dispatch")
+	  "g b" '(magit-branch-checkout :wk "Switch branch")
+	  "g c" '(:ignore t :wk "Create") 
+	  "g c b" '(magit-branch-and-checkout :wk "Create branch and checkout")
+	  "g c c" '(magit-commit-create :wk "Create commit")
+	  "g c f" '(magit-commit-fixup :wk "Create fixup commit")
+	  "g C" '(magit-clone :wk "Clone repo")
+	  "g f" '(:ignore t :wk "Find") 
+	  "g f c" '(magit-show-commit :wk "Show commit")
+	  "g f f" '(magit-find-file :wk "Magit find file")
+	  "g f g" '(magit-find-git-config-file :wk "Find gitconfig file")
+	  "g F" '(magit-fetch :wk "Git fetch")
+	  "g g" '(magit-status :wk "Magit status")
+	  "g i" '(magit-init :wk "Initialize git repo")
+	  "g l" '(magit-log-buffer-file :wk "Magit buffer log")
+	  "g r" '(vc-revert :wk "Git revert file")
+	  "g s" '(magit-stage-file :wk "Git stage file")
+	  "g t" '(git-timemachine :wk "Git time machine")
+	  "g u" '(magit-stage-file :wk "Git unstage file"))
 
-       (zg/leader-keys
-	"h" '(:ignore t :wk "Help")
-	"h f" '(describe-function :wk "Describe function")
-	"h v" '(describe-variable :wk "Describe variable")
-	"h t" '(load-theme :wk "Load theme")    
-	"h r r" '((lambda () (interactive) (load-file "~/.config/emacs/init.el")) :wk "Reload emacs config"))
+	 (zg/leader-keys
+	  "h" '(:ignore t :wk "Help")
+	  "h f" '(describe-function :wk "Describe function")
+	  "h v" '(describe-variable :wk "Describe variable")
+	  "h t" '(load-theme :wk "Load theme")    
+	  "h r r" '((lambda () (interactive) (load-file "~/.config/emacs/init.el")) :wk "Reload emacs config"))
 
-       (zg/leader-keys
-	 "m" '(:ignore t :wk "Org")
-	 "m a" '(org-agenda :wk "Org agenda")
-	 "m e" '(org-export-dispatch :wk "Org export dispatch")
-	 "m i" '(org-toggle-item :wk "Org toggle item")
-	 "m t" '(org-todo :wk "Org todo")
-	 "m B" '(org-babel-tangle :wk "Org babel tangle")
-	 "m T" '(org-todo-list :wk "Org todo list")
-	 "m x" '(org-toggle-checkbox :wk "Org toggle checkbox")
-	 "m m" '(org-roam-node-find :wk "Org Roam find node")
-	 "m I" '(org-roam-node-insert :wk "Org Roam insert node"))
+	 (zg/leader-keys
+	   "m" '(:ignore t :wk "Org")
+	   "m a" '(org-agenda :wk "Org agenda")
+	   "m e" '(org-export-dispatch :wk "Org export dispatch")
+	   "m i" '(org-toggle-item :wk "Org toggle item")
+	   "m t" '(org-todo :wk "Org todo")
+	   "m B" '(org-babel-tangle :wk "Org babel tangle")
+	   "m T" '(org-todo-list :wk "Org todo list")
+	   "m x" '(org-toggle-checkbox :wk "Org toggle checkbox")
+	   "m m" '(org-roam-node-find :wk "Org Roam find node")
+	   "m I" '(org-roam-node-insert :wk "Org Roam insert node"))
 
-       (zg/leader-keys
-	 "m b" '(:ignore t :wk "Tables")
-	 "m b -" '(org-table-insert-hline :wk "Insert hline in table"))
+	 (zg/leader-keys
+	   "m b" '(:ignore t :wk "Tables")
+	   "m b -" '(org-table-insert-hline :wk "Insert hline in table"))
 
-       (zg/leader-keys
-	 "m" '(:ignore t :wk "Org")
-	 "m a" '(org-agenda :wk "Org agenda")
-	 "m e" '(org-export-dispatch :wk "Org export dispatch")
-	 "m t" '(org-todo :wk "Org todo")
-	 "m B" '(org-babel-tangle :wk "Org babel tangle")
-	 "m T" '(org-todo-list :wk "Org todo list")
-	 "m d t" '(org-time-stamp :wk "Org time stamp"))
+	 (zg/leader-keys
+	   "m" '(:ignore t :wk "Org")
+	   "m a" '(org-agenda :wk "Org agenda")
+	   "m e" '(org-export-dispatch :wk "Org export dispatch")
+	   "m t" '(org-todo :wk "Org todo")
+	   "m B" '(org-babel-tangle :wk "Org babel tangle")
+	   "m T" '(org-todo-list :wk "Org todo list")
+	   "m d t" '(org-time-stamp :wk "Org time stamp"))
 
+ 
+	 (zg/leader-keys
+	  "c" '(:ignore t :wk "Schedule") 
+	  "c s" '(org-schedule :wk "Set Org Schedule")
+	  "c d" '(org-deadline :wk "Set Org Deadline")
+	   )
 
-       (zg/leader-keys
-	"c" '(:ignore t :wk "Schedule") 
-	"c s" '(org-schedule :wk "Set Org Schedule")
-	"c d" '(org-deadline :wk "Set Org Deadline")
-	 )
+	 (zg/leader-keys
+	   "p" '(projectile-command-map :wk "Projectile"))
 
-       (zg/leader-keys
-	 "p" '(projectile-command-map :wk "Projectile"))
+	 (zg/leader-keys
+	  "t" '(:ignore t :wk "Toggle")
+	  "t e" '(eshell-toggle :wk "Toggle eshell")
+	  "t f" '(flycheck-mode :wk "Toggle flycheck")
+	  "t l" '(display-line-numbers-mode :wk "Toggle line numbers")
+	  "t n" '(neotree-toggle :wk "Toggle neotree file viewer")
+	  "t o" '(org-mode :wk "Toggle org mode")
+	  "t r" '(rainbow-mode :wk "Toggle rainbow mode")
+	  "t t" '(visual-line-mode :wk "Toggle truncated lines")
+	  "t v" '(vterm-toggle :wk "Toggle vterm")
+	  "t d" '(darkroom-mode :wk "Toggle darkroom"))
 
-       (zg/leader-keys
-	"t" '(:ignore t :wk "Toggle")
-	"t e" '(eshell-toggle :wk "Toggle eshell")
-	"t f" '(flycheck-mode :wk "Toggle flycheck")
-	"t l" '(display-line-numbers-mode :wk "Toggle line numbers")
-	"t n" '(neotree-toggle :wk "Toggle neotree file viewer")
-	"t o" '(org-mode :wk "Toggle org mode")
-	"t r" '(rainbow-mode :wk "Toggle rainbow mode")
-	"t t" '(visual-line-mode :wk "Toggle truncated lines")
-	"t v" '(vterm-toggle :wk "Toggle vterm")
-	"t d" '(darkroom-mode :wk "Toggle darkroom"))
+	 (zg/leader-keys
+	  "s" '(:ignore t :wk "Search")
+	  "s d" '(dictionary-search :wk "Search dictionary")
+	  "s m" '(man :wk "Man pages")
+	  "s t" '(tldr :wk "Lookup TLDR docs for a command")
+	  "s w" '(woman :wk "Similar to man but doesn't require man"))
 
-       (zg/leader-keys
-	"s" '(:ignore t :wk "Search")
-	"s d" '(dictionary-search :wk "Search dictionary")
-	"s m" '(man :wk "Man pages")
-	"s t" '(tldr :wk "Lookup TLDR docs for a command")
-	"s w" '(woman :wk "Similar to man but doesn't require man"))
+	(zg/leader-keys
+	   "d" '(:ignore t :wk "Dired")
+	   "d d" '(dired :wk "Open dired")
+	   "d j" '(dired-jump :wk "Dired jump to current")
+	   "d n" '(neotree-dir :wk "Open directory in neotree")
+	   "d p" '(peep-dired :wk "Peep-dired"))
 
-      (zg/leader-keys
-	 "d" '(:ignore t :wk "Dired")
-	 "d d" '(dired :wk "Open dired")
-	 "d j" '(dired-jump :wk "Dired jump to current")
-	 "d n" '(neotree-dir :wk "Open directory in neotree")
-	 "d p" '(peep-dired :wk "Peep-dired"))
+	(zg/leader-keys
+	  "o" '(:ignore t :wk "Open")
+	  "o d" '(dashboard-open :wk "Dashboard")
+	  "o e" '(elfeed :wk "Elfeed RSS")
+	  "o f" '(make-frame :wk "Open buffer in new frame")
+	  "o F" '(select-frame-by-name :wk "Select frame by name"))
 
-      (zg/leader-keys
-	"o" '(:ignore t :wk "Open")
-	"o d" '(dashboard-open :wk "Dashboard")
-	"o e" '(elfeed :wk "Elfeed RSS")
-	"o f" '(make-frame :wk "Open buffer in new frame")
-	"o F" '(select-frame-by-name :wk "Select frame by name"))
-
-       (zg/leader-keys
-	"w" '(:ignore t :wk "Windows")
-	;; Window splits
-	"w c" '(evil-window-delete :wk "Close window")
-	"w n" '(evil-window-new :wk "New window")
-	"w s" '(evil-window-split :wk "Horizontal split window")
-	"w v" '(evil-window-vsplit :wk "Vertical split window")
-	;; Window motions
-	"w h" '(evil-window-left :wk "Window left")
-	"w j" '(evil-window-down :wk "Window down")
-	"w k" '(evil-window-up :wk "Window up")
-	"w l" '(evil-window-right :wk "Window right")
-	"w w" '(evil-window-next :wk "Goto next window")
-	;; Move Windows
-	"w H" '(buf-move-left :wk "Buffer move left")
-	"w J" '(buf-move-down :wk "Buffer move down")
-	"w K" '(buf-move-up :wk "Buffer move up")
-	"w L" '(buf-move-right :wk "Buffer move right"))
+	 (zg/leader-keys
+	  "w" '(:ignore t :wk "Windows")
+	  ;; Window splits
+	  "w c" '(evil-window-delete :wk "Close window")
+	  "w n" '(evil-window-new :wk "New window")
+	  "w s" '(evil-window-split :wk "Horizontal split window")
+	  "w v" '(evil-window-vsplit :wk "Vertical split window")
+	  ;; Window motions
+	  "w h" '(evil-window-left :wk "Window left")
+	  "w j" '(evil-window-down :wk "Window down")
+	  "w k" '(evil-window-up :wk "Window up")
+	  "w l" '(evil-window-right :wk "Window right")
+	  "w w" '(evil-window-next :wk "Goto next window")
+	  ;; Move Windows
+	  "w H" '(buf-move-left :wk "Buffer move left")
+	  "w J" '(buf-move-down :wk "Buffer move down")
+	  "w K" '(buf-move-up :wk "Buffer move up")
+	  "w L" '(buf-move-right :wk "Buffer move right"))
+	 (zg/leader-keys
+	  "a" '(:ignore t :wk "PDFs") 
+	  "a i" '(pdf-annot-add-text-annotation :wk "insert text annotation in pdf") 
+	   )
 )
 
 (use-package git-timemachine
@@ -334,6 +338,20 @@
 							       'ivy-rich-switch-buffer-transformer))
 
 (use-package drag-stuff)
+
+(use-package pdf-tools
+  :defer t
+  :commands pdf-loader-install
+  :mode "\\.pdf\\'"
+  :bind (:map pdf-view-mode-map
+			  ("J" . pdf-view-next-line-or-next-page)
+			  ("K" . pdf-view-previous-line-or-previous-page)
+			  ("C-=" . pdf-view-enlarge)
+			  ("C--" . pdf-view-shrink))
+  :init (pdf-loader-install)
+  :config (add-to-list 'revert-without-query ".pdf")
+)
+(add-hook 'pdf-view-mode-hook (lambda () (display-line-numbers-mode -1)))
 
 (use-package toc-org
 	:commands toc-org-enable
@@ -547,17 +565,15 @@
       ;; Corrects (and improves) org-mode's native fontification.
       (doom-themes-org-config))
 
-(add-to-list 'default-frame-alist '(alpha-background . 90)) ; For all new frames henceforth
-
 (use-package doom-modeline
-      :ensure t
-      :init (doom-modeline-mode 1)
-      :config
-      (setq doom-modeline-height 35      ;; sets modeline height
-		doom-modeline-bar-width 5    ;; sets right bar width
-		doom-modeline-persp-name t   ;; adds perspective name to modeline
-		doom-modeline-persp-icon t
-		doom-modeline-enable-word-count t)) ;; adds folder icon next to persp name
+	:ensure t
+	:init (doom-modeline-mode 1)
+	:config
+	(setq doom-modeline-height 35      ;; sets modeline height
+		  doom-modeline-bar-width 5    ;; sets right bar width
+		  doom-modeline-persp-name t   ;; adds perspective name to modeline
+		  doom-modeline-persp-icon t
+		  doom-modeline-enable-word-count t)) ;; adds folder icon next to persp name
 
 (use-package spacious-padding)
 (require 'spacious-padding)
@@ -582,21 +598,32 @@
 (define-key global-map (kbd "<f8>") #'spacious-padding-mode)
 
 (use-package dashboard
-      :ensure t 
-      :init
-      (setq initial-buffer-choice 'dashboard-open)
-      (setq dashboard-set-heading-icons t)
-      (setq dashboard-set-file-icons t)
-      (setq dashboard-startup-banner "/home/nightwing/.config/emacs/images/emacs-dash.txt")  ;; use custom image as banner
-      (setq dashboard-center-content t) ;; set to 't' for centered content
-      (setq dashboard-items '((recents . 5)
-						      (agenda . 3)
-						      (bookmarks . 3)
-						      (projects . 3)))
-      :custom
-      (dashboard-modify-heading-icons '((recents . "file-text")
-									(bookmarks . "book")))
-      :config
-      (dashboard-setup-startup-hook))
+	:ensure t 
+	:init
+	(setq initial-buffer-choice 'dashboard-open)
+	(setq dashboard-set-heading-icons t)
+	(setq dashboard-set-file-icons t)
+	;;(setq dashboard-startup-banner "/home/nightwing/.dots/.config/emacs/images/emacs-dash.txt")  ;; use custom image as banner
+	(setq dashboard-startup-banner 'logo)
+	(setq dashboard-center-content t) ;; set to 't' for centered content
+	(setq dashboard-items '((recents . 5)
+							(agenda . 3)
+							(bookmarks . 3)
+							(projects . 3)))
+	:custom
+	(dashboard-modify-heading-icons '((recents . "file-text")
+									  (bookmarks . "book")))
+	:config
+	(dashboard-setup-startup-hook))
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-startup-banner "/home/nightwing/.config/emacs/images/emacs-dash.txt")
+(setq inhibit-startup-screen t)
+)
 
 (use-package perspective)
+
+(add-to-list 'default-frame-alist '(alpha-background . 90)) ; For all new frames henceforth
