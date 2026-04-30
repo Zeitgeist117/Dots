f8f1bc832660ac6fa0fb62f999d56c46def4d194
Zeitgeist117 <zeitgeist117@proton.me>
2026-04-30 20:23:49 +1000

    revert emacs doom-modeline

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,22 +1,14 @@
 (add-to-list 'load-path "~/.config/emacs/scripts/")
 (org-babel-load-file
  (expand-file-name
-  "[config.org](http://config.org)"
+  "config.org"
   user-emacs-directory))
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-language-environment 'utf-8)
 (set-selection-coding-system 'utf-8)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(package-vc-selected-packages
    '((book-mode :url "https://github.com/rougier/book-mode.git"))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
