3058d6e75fb4d10f0c1fa7122aa6750581a210b6
Zeitgeist117 <zeitgeist117@proton.me>
2026-06-04 03:45:16 +1000

    readme updates

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -7,8 +7,3 @@
 (set-default-coding-systems 'utf-8)
 (set-language-environment 'utf-8)
 (set-selection-coding-system 'utf-8)
-(custom-set-variables
- '(package-vc-selected-packages
-   '((book-mode :url "https://github.com/rougier/book-mode.git"))))
-(custom-set-faces
- )
